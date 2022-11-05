54debfb78309476e47f4222873221fe1c6b8dbf6
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-11-06 01:06:39 +0300

    add home pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MovieCard from "../component/MovieCard";
+
+const Home = () => {
+  return (
+    <>
+      <MovieCard />
+    </>
+  );
+};
+
+export default Home;
